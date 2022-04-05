669a2d0efb1aaec5d55074af549a103287ed87da
asael <levinasael@gmail.com>
2022-04-05 19:42:09 +0300

    add class and shuffle

--- maine.js
+++ maine.js
@@ -20,10 +20,18 @@ const cards = [
   "i",
   "j",
 ];
+
 function addCards(v, i, arr) {
   const element = document.createElement("div");
   element.innerHTML = v;
+  element.id = i;
+  element.className= "card"
   const board = document.getElementById("board");
   board.appendChild(element);
 }
+shuffleCards(cards);
 cards.forEach(addCards);
+
+function shuffleCards(v, i, arr) {
+  cards.sort(() => (Math.random() > 0.5 ? 1 : -1));
+}
